4bf4bc1d82bc9570fb3a6522abcb26722fc0ecd7
Denys <d.bezuhlyi@devatlant.com>
2019-06-25 22:10:48 +0300

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+require('../css/app.scss');
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+
+$(document).ready(function() {
+    // $('.carousel').carousel();
+    console.log('hello');
+});
